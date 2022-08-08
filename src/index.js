b0c8a7d9f3803e435141182bb0c4a509f6e8d46c
usman.shahid@grp.contegris.com <b1lFXW6#i@f_>
2022-08-08 16:38:12 -0700

    router update

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM          from 'react-dom/client';
 import App               from "./App"
 import store             from "./components/store"
 import { Provider }      from 'react-redux';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={ store }>
-    <BrowserRouter>
+    <Router>
       <App/>
-    </BrowserRouter>
+    </Router>
   </Provider>
 );
