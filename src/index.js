439447bf7230d44dc052d20a753df16cfa6b2a43
usmanshahid5 <108838496+usmanshahid5@users.noreply.github.com>
2022-08-08 16:30:00 -0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={ store }>
-    <BrowserRouter basename="/MyCarRentalSite/">
+    <BrowserRouter>
       <App/>
     </BrowserRouter>
   </Provider>
