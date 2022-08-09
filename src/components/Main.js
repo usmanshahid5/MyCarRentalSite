8b995b11a56a4c5fea45af434197ddee81f90067
usman.shahid@grp.contegris.com <b1lFXW6#i@f_>
2022-08-08 18:03:48 -0700

    Change check

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,5 @@
 import React             from 'react'
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, Switch } from "react-router-dom";
 import Header            from '../header/Header';
 import AboutUs           from './Aboutus/AboutUs';
 import AllCars           from './AllCars/AllCars';
