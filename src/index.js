34939bc58cf02c9c89f61c7709a7e4a29fc184df
usman.shahid@grp.contegris.com <b1lFXW6#i@f_>
2022-08-10 15:52:19 -0700

    Commit Index.js Commit

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import App               from "./App"
 import store             from "./components/store"
 import { Provider }      from 'react-redux';
 import { HashRouter, Link } from "react-router-dom";
+import Header from "./header/Header"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={ store }>
     <HashRouter>
-      <App/>
+      {/*<App/>*/}
+      <Header/>
     </HashRouter>
   </Provider>
 );
